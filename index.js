b42e4e2b6076c64b663c01464521ed49722c6837
LqosL <camilla_agliullina@mail.ru>
2023-12-01 22:56:14 +0300

    feat: added success form validation, burger swap

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ function checkIfEmpty (form) {
 }
 
 function validatePhone (phone) {
-    const regex = /((\+7)|8)[3489][0-9]{9}/
-    return regex.test(phone);
+    const gapless = phone.replaceAll(' ', '');
+    const regexP7 = /\+7[3489][0-9]{9}/;
+    const regex8 = /8[3489][0-9]{9}/;
+    return regexP7.test(gapless) || regex8.test(gapless);
 }
 
 function phoneMask(element, string) {
@@ -21,8 +23,8 @@ function phoneMask(element, string) {
         let result = '';
         const letterless = string.replaceAll(/[^\d+]/g, '')
         const gapless =  letterless.replaceAll(' ', '')
-        if (gapless.length > 12) {
-            result = gapless.slice(0, 12)
+        if ((gapless.length > 11 && gapless.startsWith('8')) || (gapless.length > 12 && gapless.startsWith('+7'))) {
+            result = gapless.slice(0, -1)
         } else {
             result = gapless;
         }
@@ -50,7 +52,7 @@ function phoneMask(element, string) {
             return (string.substring(0,2) + ' ' + string.substring(2, 5)  + ' ' + string.substring(5, 8) + ' ' + string.substring(8, 10) + ' ' + string.substring(10)).trim();
         }
         if (string.slice(0,1) === '8') {
-            return (string.substring(0,1) + ' ' + string.substring(1, 4)  + ' ' + string.substring(4, 8) + ' ' + string.substring(8, 10) + ' ' + string.substring(10)).trim();
+            return (string.substring(0,1) + ' ' + string.substring(1, 4)  + ' ' + string.substring(4, 7) + ' ' + string.substring(7, 9) + ' ' + string.substring(9)).trim();
         }
         return ''
     }
@@ -58,8 +60,8 @@ function phoneMask(element, string) {
 }
 
 function validateForm(form) {
+    let isValid = true;
     const error_message = document.getElementsByClassName('contact_error-message')[0];
-
     const required = Array.from(form.getElementsByClassName('required'));
     required.forEach((block)=>{
         const value = block.value;
@@ -67,6 +69,7 @@ function validateForm(form) {
         if (value.length === 0) {
             siblings.item(siblings.length -1 ).classList.remove('hidden');
             error_message.classList.remove('hidden');
+            isValid = false;
         } else {
             siblings.item(siblings.length -1 ).classList.add('hidden');
         }
@@ -78,13 +81,23 @@ function validateForm(form) {
     if (phone_input.value.length === 0) {
         phoneError.innerText = 'This field is required';
         phoneError.classList.remove('hidden');
+        isValid = false;
         return;
     }
     if (!validatePhone(phone_input.value)) {
         phoneError.innerText = 'incorrect number';
         phoneError.classList.remove('hidden');
+        isValid = false;
     } else {
         phoneError.classList.add('hidden');
+    }
+
+    if(isValid) {
+        console.log('all right')
+        const form_box = document.getElementsByClassName('contact__box')[0];
+        form_box.classList.add('hidden');
+        const thanks_message = document.getElementsByClassName('contact__thanks-message')[0];
+        thanks_message.classList.remove('hidden');
     }
 }
 
@@ -133,13 +146,37 @@ window.addEventListener('load', ()=> {
         })
     })
 
-    submit_button.addEventListener('click', ()=> {
-        validateForm(form);
-    })
+
 
     const phone_input = form.getElementsByClassName('phone_input')[0];
     phone_input.addEventListener('input', ()=> {
         phoneMask(phone_input, phone_input.value)
+    })
+
+    const thanks_window = document.getElementsByClassName('contact__thanks-message')[0];
+    const close__thanks = thanks_window.getElementsByClassName('contact__close-btn')[0];
+     close__thanks.addEventListener('click', () => {
+         thanks_window.classList.add('hidden');
+     })
+    const close__thanks_lower = thanks_window.getElementsByClassName('contact__thanks-button')[0];
+    close__thanks_lower.addEventListener('click', () => {
+        thanks_window.classList.add('hidden');
+    })
+
+    submit_button.addEventListener('click', ()=> {
+        validateForm(form);
+    })
+
+
+    const burger = document.getElementsByClassName('burger')[0];
+    const burger_close = document.getElementsByClassName('burger__close-btn')[0];
+    const burger_box = document.getElementsByClassName('burger__menu')[0];
+
+    burger.addEventListener('click', () => {
+        burger_box.classList.remove('hidden');
+    })
+    burger_close.addEventListener('click', () => {
+        burger_box.classList.add('hidden');
     })
 });
 
