136a50412715ded7731d9f918cf6bc75f611aab2
LqosL <camilla_agliullina@mail.ru>
2023-12-02 00:40:06 +0300

    feat: add basic slider landing

---
+++ slider.js
@@ -0,0 +1,23 @@
+const Directions = {
+    0: "FORWARD",
+    1: "BACKWARD"
+}
+
+function backgroundChanger(currentLink, direction) {
+
+}
+
+
+
+const picturesLinks = [
+    'assets/slider_images/0.jpg',
+    'assets/slider_images/1.jpg',
+    'assets/slider_images/2.jpg',
+    'assets/slider_images/3.jpg',
+    'assets/slider_images/4.jpg',
+    'assets/slider_images/5.jpg',
+    'assets/slider_images/6.jpg',
+    'assets/slider_images/7.jpg',
+    'assets/slider_images/8.jpg',
+    'assets/slider_images/9.jpg',
+]
